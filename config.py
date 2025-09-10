60255585b30749434bfc5d4ce3834a294799b89e
ramusharma00 <ramusharma8190@gmail.com>
2025-09-10 14:37:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     pass
     """
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8457366280:AAGATQ6Rwo61KjHMZltM8Q3S1OfQxw84_Fg")
-    API_ID = int(os.environ["API_ID", 301896416591]
-    API_HASH = os.environ["API_HASH", "8d5898281c59b9aa3a3018964165913b"]
+    API_ID = int(os.environ["API_ID", 29946578]
+    API_HASH = os.environ["API_HASH", "57e0b762f105ab1db072fabe4d65114b"]
     AUTH_USERS = "6722252412"
 # 
